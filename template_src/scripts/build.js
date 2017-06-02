3670c334327e90111b8f079b1aaf655c89c700a0
Omar Gonzalez <ogonzalez2212@gmail.com>
2017-06-02 17:36:14 -0500

    Build error fixed

--- template_src/scripts/build.js
+++ template_src/scripts/build.js
@@ -10,18 +10,3 @@ browserify('www/src/main.js')
   .transform(vueify)
   .bundle()
   .pipe(fs.createWriteStream('www/dist/build.js'))
-
-watch.watchTree('../www', function (f, curr, prev) {
-    if (typeof f == "object" && prev === null && curr === null) {
-    } else if (prev === null) {
-	// f is a new file
-	console.log("New file: "+f);
-    } else if (curr.nlink === 0) {
-      	// f was removed
-	console.log("File: "+f+" removed.");
-    } else {
-      	// f was changed
-	console.log("File: "+f+" changed.");
-    }
-     	exec("watchify"); 
-})
